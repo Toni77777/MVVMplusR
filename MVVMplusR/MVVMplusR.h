6301f4b0910c04235059c84a479a5c2901c86587
Anton Paliakov <toxa95401@gmail.com>
2020-05-16 14:26:09 +0300

    Add base project.

---
+++ MVVMplusR/MVVMplusR.h
@@ -0,0 +1,19 @@
+//
+//  MVVMplusR.h
+//  MVVMplusR
+//
+//  Created by Anton Polyakov on 5/16/20.
+//  Copyright © 2020 Anton Paliakou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MVVMplusR.
+FOUNDATION_EXPORT double MVVMplusRVersionNumber;
+
+//! Project version string for MVVMplusR.
+FOUNDATION_EXPORT const unsigned char MVVMplusRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVVMplusR/PublicHeader.h>
+
+
